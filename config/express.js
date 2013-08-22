8b5d30929ee77ce6cac71ee57b7703d5aedd63fa
Kuntau <kuntau17@gmail.com>
2013-08-23 04:41:33 +0800

    modified:   config/express.js
    	remove the need to use nib and simplify stylus converting using
    	grunt

--- config/express.js
+++ config/express.js
@@ -1,6 +1,4 @@
-var express = require('express'),
-    stylus  = require('stylus'),
-    nib  = require('nib');
+var express = require('express');
 
 module.exports = function(app, config) {
   app.configure(function () {
@@ -8,23 +6,13 @@ module.exports = function(app, config) {
     app.use(express.static(config.root + '/public'));
     app.set('port', config.port);
     app.set('views', config.root + '/app/views');
-    app.engine('html', require('jade').__express);
-    app.set('view engine', 'html');
+    app.set('view engine', 'jade');
     app.use(express.favicon(config.root + '/public/img/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use(stylus.middleware({
-      src: config.root + 'app/views/style',
-      dest: config.root + 'public/css',
-      compile: function (str, path) {
-        stylus(str)
-        .set('filename', path)
-        .set('compress', true)
-        .use(nib());
-      }
-    }));
+    app.use(require('stylus').middleware( config.root + '/app/views/style'));
     app.use(function(req, res) {
       res.status(404).render('404', { title: '404' });
     });
