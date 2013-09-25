11d2b6899ba115db931dc4a82576adb0fb1131dd
Kuntau <kuntau17@gmail.com>
2013-09-25 22:07:17 +0800

    some layout change. refer to example http://getbootstrap.com/examples/jumbotron/

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+    Article = mongoose.model('Article');
 
 exports.index = function(req, res){
   Article.find(function(err, articles){
